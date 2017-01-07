a25528f2bbe61bfdf63ead1356541ee93bbb3244
Mark Herhold <mark.herhold@gmail.com>
2017-01-07 14:46:28 -0500

    adding getAchievementCriteria(), getAchievementAwards()

--- src/achievements/index.js
+++ src/achievements/index.js
@@ -1,10 +1,39 @@
 'use strict';
 
+const check = require('check-types');
 const common = require('./../common');
 const ENDPT = 'achievements';
 
 // Achievements module
 // @param context: The context to make requests in. Basically, `this`
 module.exports = function achievements(context) {
-    return common(context, ENDPT);
+    const obj = common(context, ENDPT);
+
+    // retrieves a list of criteria
+    // @param id ID of the achievement to retrieve criteria for
+    // @param userOpts: option overrides for this request
+    // @returns Returns a promise that resolves with the list of criteria
+    function getAchievementCriteria(id, userOpts) {
+        check.string(id, 'id must be a string');
+
+        return context.http.makeRequest({
+            url: `/v1/apps/${context.applicationId}/${ENDPT}/${id}/criteria`
+        }, userOpts).then(function(body) { return body.data; });
+    }
+
+    // retrieves a list of awards
+    // @param id ID of the achievement to retrieve criteria for
+    // @param userOpts: option overrides for this request
+    // @returns Returns a promise that resolves with the list of awards
+    function getAchievementAwards(id, userOpts) {
+        check.string(id, 'id must be a string');
+
+        return context.http.makeRequest({
+            url: `/v1/apps/${context.applicationId}/${ENDPT}/${id}/awards`
+        }, userOpts).then(function(body) { return body.data; });
+    }
+
+    obj.getAchievementCriteria = getAchievementCriteria;
+    obj.getAchievementAwards = getAchievementAwards;
+    return obj;
 };
