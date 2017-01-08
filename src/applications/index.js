12e7d7c4993228b1a066ded0002c5db8305d0b52
Mark Herhold <mark.herhold@gmail.com>
2017-01-08 13:11:36 -0500

    Adding application create method

--- src/applications/index.js
+++ src/applications/index.js
@@ -1,18 +1,32 @@
 'use strict';
 
+const check = require('check-types');
 const pageToGenerator = require('./../utils/pageToGenerator');
 const ENDPT = 'apps';
 
 // API Keys module
 // @param context: The context to make requests in. Basically, `this`
-module.exports = function apiKeys(context) {
+module.exports = function applications(context) {
+    // create an application
+    // @param object
+    // @param userOpts: option overrides for this request
+    // @return An iterator that returns promises that resolve with the next object
+    function create(object, userOpts) {
+        check.object(object, 'object must be an object');
+
+        return context.http.makeRequest({
+            method: 'POST',
+            body: object,
+            url: `/v1/${ENDPT}`
+        }, userOpts);
+    }
 
     // retrieve all applications
     // @param userOpts: option overrides for this request
     // @return An iterator that returns promises that resolve with the next object
     function* getAll(userOpts) {
         function pageFn() {
-            let url = `/v1/${ENDPT}/`;
+            let url = `/v1/${ENDPT}`;
             return function() {
                 return context.http.makeRequest({ url }, userOpts).then(function(body) {
                     url = body.pages.next;
@@ -25,6 +39,7 @@ module.exports = function apiKeys(context) {
     }
 
     return {
-        getAll
+        getAll,
+        create
     };
 };
