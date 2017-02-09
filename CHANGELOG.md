4bddc48da6e76b426b93fac0be98eb7e31e61665
Robert Herhold <RobertHerhold@users.noreply.github.com>
2017-02-09 16:54:17 -0500

    Add criteria.getDynamicCriteriaImages() (#3)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/) and this p
 ### Added
 - applications.getAll()
 - apikeys.getIterator()
+- criteria.getDynamicCriteriaImages()
 
 ### Changed
 - N/A
--- src/criteria/index.js
+++ src/criteria/index.js
@@ -6,5 +6,24 @@ const ENDPT = 'criteria';
 // Criterion module
 // @param context: The context to make requests in. Basically, `this`
 module.exports = function criteria(context) {
-    return common(context, ENDPT);
+    const obj = common(context, ENDPT);
+
+    // Get a list of all dynamic criteria runtimes and image tags
+    // @param userOpts: option overrides for this request
+    // @returns Returns a promise that resolves with the requested dynamic criteria images
+    function getDynamicCriteriaImages(userOpts) {
+        return context.http.makeRequest({
+            url: `/v1/apps/${context.applicationId}/dcimages`
+        }, userOpts);
+    }
+
+    return {
+        get: obj.get,
+        getIterator: obj.getIterator,
+        getAll: obj.getAll,
+        create: obj.create,
+        update: obj.update,
+        remove: obj.remove,
+        getDynamicCriteriaImages
+    };
 };
