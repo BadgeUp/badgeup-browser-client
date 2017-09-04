f7594e830c04d4e03de6d6dd8b0cc992b51ebcbb
Mark Herhold <mark.herhold@gmail.com>
2017-09-04 17:01:26 -0400

    revert bad changes to the fetch wrapper, remove simple options

--- src/utils/fetchWrapper.js
+++ src/utils/fetchWrapper.js
@@ -9,14 +9,19 @@ const fetchWrapper = function fetchWrapper(options) {
         throw new Error('options.baseUrl or options.url must be provided and must be a string');
     }
 
+    // stringify object and array bodies
+    if (options.json === true && options.body && (typeof options.body === 'object' || Array.isArray(options.body))) {
+        options.headers = options.headers || {};
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(options.body);
+    }
+
     // build the URL
     let url = options.url || '';
     url = options.baseUrl ? options.baseUrl + url : url;
     delete options.baseUrl;
     delete options.url;
 
-    const simple = (options.simple === undefined) ? true : false;
-    delete options.simple;
     return fetch(url, options).then(function(response) {
         if (!response.ok) {
             return Promise.reject(response);
